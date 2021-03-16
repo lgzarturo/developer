b872554df7bc9aeced6f50c9f54f1a11fd925c6a
Arturo López <lgzarturo@gmail.com>
2021-03-16 18:18:17 -0500

    footer links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,10 +9,9 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
-          </a> - <a href="https://arthurolg.com">blog arthurolg.com</a>
+          <a href="https://arthurolg.com">blog profesional</a>{" "}
+          <a href="https://webdeveloperhints.com">webdeveloperhints.com</a>{" "}
+          <a href="https://miraeljuego.com">miraeljuego.com</a>
         </p>
       </div>
     </Fade>
